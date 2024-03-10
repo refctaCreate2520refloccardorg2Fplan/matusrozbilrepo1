eba463eb4f8169ba20e14fb8bc6c5d061a53df07
Matúš Nekoranec <matusnekoranec22@gmail.com>
2024-03-10 16:51:23 +0100

    added deadline into dtos and model

--- AspNetWebAPI/DTO/CreateTaskDTO.cs
+++ AspNetWebAPI/DTO/CreateTaskDTO.cs
@@ -5,5 +5,6 @@
         public string taskName { get; set; } = default!;
         public string taskDescription { get; set; } = default!;
         public int taskPriority { get; set; } = default!;
+        public DateOnly taskDeadline { get; set; } = default!; 
     }
 }
--- AspNetWebAPI/DTO/TasksDTO.cs
+++ AspNetWebAPI/DTO/TasksDTO.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public int Priority { get; set; }
         public bool IsDone { get; set; }
+        public DateOnly Deadline { get; set; }
     }
 }
