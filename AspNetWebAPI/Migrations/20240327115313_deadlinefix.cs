3237876235be6512990305d16358fe88c00106ec
Marko Jantula <jantulamarko@outlook.com>
2024-03-27 13:40:56 +0100

    forms style

---
+++ AspNetWebAPI/Migrations/20240327115313_deadlinefix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class deadlinefix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Deadline",
+                table: "Tasks",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Tasks");
+        }
+    }
+}
