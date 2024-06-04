d939d1819584efb3f6b3dca8d0edb9de8fd954bd
Marko Jantula <jantulamarko@outlook.com>
2024-06-04 08:56:02 +0200

    rxjs time layout edit
    - TODO: searchbar design a celkovo

--- angular-project/src/app/dashboard/dashboard.component.ts
+++ angular-project/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TestService } from '../test.service';
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, NgForOf, NgIf } from '@angular/common';
 import { MatList, MatListItem } from '@angular/material/list';
 import { MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable, MatTableDataSource } from '@angular/material/table';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject, Subscription, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     NgIf,
+    CommonModule,
     AsyncPipe,
     MatList,
     MatListItem,
@@ -29,13 +31,22 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  private testService = inject(TestService);
+  time = new Date();
+  private destroy$ = new Subject<void>();
+  timeSubscription: Subscription;
+  ngOnInit() {
+    const timeSource = timer(0, 1000);
+    this.timeSubscription = timeSource.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.time = new Date();
+    });
+  }
 
-  dataSource: MatTableDataSource<string>;
 
-  ngOnInit(): void {
-    this.testService.getNames()
-      .pipe(takeUntilDestroyed())
-      .subscribe(names => this.dataSource = new MatTableDataSource(names));
+  ngOnDestroy() {
+    if (this.timeSubscription) {
+      this.timeSubscription.unsubscribe();
+    }
   }
 }
--- angular-project/src/app/main-nav/main-nav.component.ts
+++ angular-project/src/app/main-nav/main-nav.component.ts
@@ -3,13 +3,15 @@ import { Router, RouterLink } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButton } from '@angular/material/button';
 import { AuthenticationService } from '../api-authorization/authentication.service';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { Subject, Subscription, takeUntil, timer } from 'rxjs';
 
 @Component({
   selector: 'app-main-nav',
   standalone: true,
   imports: [
     RouterLink,
+    CommonModule,
     MatToolbar,
     MatButton,
     NgIf
@@ -24,5 +26,25 @@ export class MainNavComponent {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+
+  time = new Date();
+  private destroy$ = new Subject<void>();
+  timeSubscription: Subscription;
+  ngOnInit() {
+    const timeSource = timer(0, 1000);
+    this.timeSubscription = timeSource.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.time = new Date();
+    });
+  }
+
+
+  ngOnDestroy() {
+    if (this.timeSubscription) {
+      this.timeSubscription.unsubscribe();
+    }
   }
 }
