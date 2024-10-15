20f1f0013d3ddbb4a115f74871a777ee617f733f
jantu <realthemakiii@gmail.com>
2024-10-15 08:09:26 +0200

    databazka oprava aspon pokus

--- AspNetWebAPI/Program.cs
+++ AspNetWebAPI/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddCors(options =>
         name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+            policy.WithOrigins("http://localhost:4200", "https://horizontasks-106f2.web.app").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
         });
 });
 
